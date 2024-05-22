ac40ec5df4fc34595144dd39c7bdf315c4938981
Vincent Kiathadi <vincentkiathadi@gmail.com>
2024-05-22 14:51:10 +0800

    event

--- mimpiindonesiaweb/src/Component/Event/EventDisplay.js
+++ mimpiindonesiaweb/src/Component/Event/EventDisplay.js
@@ -8,7 +8,10 @@ const EventDisplay = (props)=>{
             return theevent.map((item)=>{
                 return(
                     <span>    
-                        This is Event List                
+                        This is Event List       
+                        {item.name}<br></br>
+                        <img src={item.image}/><br></br>
+                        {item.effect}         
            
                     </span>
 
