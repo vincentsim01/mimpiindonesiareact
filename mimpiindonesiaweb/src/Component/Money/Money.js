2ebadee5a9ed19b8b5dda97e7a62919acebb9a3f
Vincent Kiathadi <vincentkiathadi@gmail.com>
2024-05-24 16:29:40 +0800

    money added

--- mimpiindonesiaweb/src/Component/Money/Money.js
+++ mimpiindonesiaweb/src/Component/Money/Money.js
@@ -1,14 +1,22 @@
 import React, { useState,useEffect } from 'react';
 import MoneyDisplay from './MoneyDisplay';
 
+const baseUrl="http://localhost:9120";
+
 
 const Money = () =>{
+
+    const [money,setmoney]= useState([]);
+
+    fetch(`${baseUrl}/Money` ,{method:'GET'})
+    .then((res) => res.json())
+    .then((data) => setmoney(data))
 
 
     return(
         <>
         This is Money
-        <MoneyDisplay/>
+        <MoneyDisplay themoney={money}/>
         
         </>
     )
--- mimpiindonesiaweb/src/Component/Money/MoneyDisplay.js
+++ mimpiindonesiaweb/src/Component/Money/MoneyDisplay.js
@@ -2,12 +2,27 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 
-const MoneyDisplay = () =>{
+const MoneyDisplay = (props) =>{
+    const MoneyList = ({themoney}) =>{
+
+        if (themoney){
+            return themoney.map((item) => {
+                return (
+                    <>
+                    {item.id}<br></br>
+                    <img src={item.image}/>
+                    
+                    </>
+                )
+            })
+        }
+    }
 
     return(
 
         <>
         This is MoneyDisplay
+        {MoneyList(props)}
         
         
         </>
