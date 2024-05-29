381fbd0e1bf9acf6998052c3f741635e3c6141a4
Vincent Kiathadi <vincentkiathadi@gmail.com>
2024-05-29 22:45:30 +0800

    header update

--- mimpiindonesiaweb/src/Component/Header.js
+++ mimpiindonesiaweb/src/Component/Header.js
@@ -1,20 +1,21 @@
 import React, { useState,useEffect } from 'react';
 import {useNavigate,Link} from 'react-router-dom';
 import Logo from '../Component/Logo/Logo';
+import './Header.css';
 
 
 const Header = () =>{
 
     return(
-        <div>
-           <Link to="/"> <span>Home</span></Link>
-            <span>About</span>
-            <Link to="/Character"><span>Character</span></Link>
-            <Link to="/Dream"><span>Dream</span></Link>
-            <Link to="/Event"><span>Event</span></Link>
-            <Link to="/Actions"><span>Action</span></Link>
-            <Link to="/Money"><span>Money</span></Link>
-            <Link to="/"><Logo/></Link>
+        <div className="HeaderContainer">
+           <Link to="/"> <div className="HeaderContent">Home</div></Link>
+           <div className="HeaderContent">About</div>
+            <Link to="/Character"><div className="HeaderContent">Character</div></Link>
+            <Link to="/Dream"><div className="HeaderContent">Dream</div></Link>
+            <Link to="/Event"><div className="HeaderContent">Event</div></Link>
+            <Link to="/Actions"><div className="HeaderContent">Action</div></Link>
+            <Link to="/Money"><div className="HeaderContent">Money</div></Link>
+            <Link to="/"><div className="HeaderContent"><Logo/></div></Link>
             
         </div>
     )
