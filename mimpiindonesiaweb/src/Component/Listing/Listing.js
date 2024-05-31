e9fe706b510a9c8c66894bb879508859d251bd2d
Vincent Kiathadi <vincentkiathadi@gmail.com>
2024-05-31 21:01:15 +0800

    listing draft

---
+++ mimpiindonesiaweb/src/Component/Listing/Listing.js
@@ -0,0 +1,16 @@
+
+import React, { useState,useEffect } from 'react';
+const baseUrl = "http://localhost:9120";
+
+
+Const Listing = () =>{
+    const [category,setcategory]=useState([]);
+
+
+    return(
+        <>
+        </>
+    )
+}
+
+export default Listing;
