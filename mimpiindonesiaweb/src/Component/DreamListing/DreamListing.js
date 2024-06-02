13b98fac4507be4e6121f3bd2a41048193bc9c1a
Vincent Kiathadi <vincentkiathadi@gmail.com>
2024-06-02 22:46:41 +0800

    dream listing

--- mimpiindonesiaweb/src/Component/DreamListing/DreamListing.js
+++ mimpiindonesiaweb/src/Component/DreamListing/DreamListing.js
@@ -1,10 +1,22 @@
 import React, { useState,useEffect } from 'react';
 import {useParams,useSearchParams,useNavigate,Link, Navigate} from 'react-router-dom';
+import DreamListingDisplay from './DreamListingDisplay';
 
 const baseUrl = "http://localhost:9120";
 
 
 const DreamListing = () => {
+
+    const [category,setcategory]=useState([]);
+    let [searchParams] = useSearchParams();
+
+    let cardnamequery = searchParams.getAll('name');
+
+    useEffect(()=>{
+        fetch(`${baseUrl}/dream?name=${cardnamequery}`,{method:'GET'})
+        .then((res)=>res.json())
+        .then((data)=>setcategory(data))
+},[])
 
 
 
@@ -12,6 +24,7 @@ const DreamListing = () => {
         <>
 
         <div>This is dreamlisting</div>
+        <DreamListingDisplay cardname={category}/>
         
         </>
     )
--- mimpiindonesiaweb/src/Component/DreamListing/DreamListingDisplay.js
+++ mimpiindonesiaweb/src/Component/DreamListing/DreamListingDisplay.js
@@ -1,2 +1,37 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+
+
+const DreamListingDisplay = (props) =>{
+    const DreamListingList = ({cardname})=> {
+        if (cardname){
+            return cardname.map((item)=>{
+                return (
+                    <>
+                    {item.name}
+                    <br></br>
+                    <img src={item.image}/>
+                    
+                    </>
+                )
+
+            })
+        }
+    }
+
+
+    return(
+
+        <>
+
+        {DreamListingList(props)}
+        
+        
+        
+        
+        </>
+    )
+}
+
+
+export default DreamListingDisplay;
