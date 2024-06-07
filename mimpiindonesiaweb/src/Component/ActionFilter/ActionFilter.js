0256491ce5442452a0f16162db0546dfdc3e6e76
Vincent Kiathadi <vincentkiathadi@gmail.com>
2024-06-07 14:08:58 +0800

    action filter work

--- mimpiindonesiaweb/src/Component/ActionFilter/ActionFilter.js
+++ mimpiindonesiaweb/src/Component/ActionFilter/ActionFilter.js
@@ -6,13 +6,13 @@ const baseUrl = "http://localhost:9120";
 const ActionFilter = (props) =>{
 
     const handleChangePhase = (event) =>{
-        fetch(`baseUrl/actionfilter?phase=${event.target.value}`, {method: 'GET'})
+        fetch(`${baseUrl}/actionfilter?phase=${event.target.value}`, {method: 'GET'})
         .then((res)=>res.json())
         .then((data)=> props.actionfiltering(data))
     }
 
     const handleChangeDuration = (event) =>{
-        fetch(`baseUrl/actionfilter?duration=${event.target.value}`, {method: 'GET'})
+        fetch(`${baseUrl}/actionfilter?duration=${event.target.value}`, {method: 'GET'})
         .then((res)=>res.json())
         .then((data)=> props.actionfiltering(data))
     }
@@ -21,9 +21,10 @@ const ActionFilter = (props) =>{
 
     return(
         <>
+        <div>
         Phase
         <select onChange={handleChangePhase} type="radio">
-            <option value="own turn">Own Turn</option>
+            <option value="own turn">own Turn</option>
             <option value="anytime">anytime</option>
         </select>
         <br></br>
@@ -33,6 +34,7 @@ const ActionFilter = (props) =>{
             <option value="instant">instant</option>
             <option value="multi turn">multi turn</option>
         </select>
+        </div>
         
         
         
