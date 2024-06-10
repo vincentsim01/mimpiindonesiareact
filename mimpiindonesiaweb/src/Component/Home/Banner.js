b6a8fdf80d52ad571c015db8e9299060d993ea25
Vincent Kiathadi <vincentkiathadi@gmail.com>
2024-06-10 22:46:24 +0800

    banner updated

--- mimpiindonesiaweb/src/Component/Home/Banner.js
+++ mimpiindonesiaweb/src/Component/Home/Banner.js
@@ -1,5 +1,6 @@
 import {Link} from 'react-router-dom';
 import React, { useState,useEffect } from 'react';
+import './Banner.css';
 
 
 const Banner = () =>{
@@ -10,11 +11,9 @@ const Banner = () =>{
         <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="https://i.ibb.co/mSx4gWS/Screenshot-2024-05-29-222015.png" alt="First slide"></img>
+      <img class="d-block w-100" src="https://i.ibb.co/f486XLY/cover-1.png" alt="First slide"></img>
     </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="https://i.ibb.co/b3ndz7x/Artboard-1-3x.png" alt="Second slide"></img>
-    </div>
+
 
   </div>
 </div>
