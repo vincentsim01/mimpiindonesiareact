fcf6652fb8c209012c76a0d5b1b3c74b0cf1b0de
Vincent Kiathadi <vincentkiathadi@gmail.com>
2024-06-26 08:26:48 +0800

    test

--- mimpiindonesiaweb/src/Component/Test/test.js
+++ mimpiindonesiaweb/src/Component/Test/test.js
@@ -1,20 +1,16 @@
 import React,{useState} from 'react';
 
 
-const Test = () =>{
+const Test = () => {
+    const [items, setItems] = useState(['Item 1', 'Item 2', 'Item 3']);
 
-    const MyComponent = () => {
-        const [items, setItems] = useState(['Item 1', 'Item 2', 'Item 3']);
-    
-        const addItem = () => {
-            const newItem = `Item ${items.length + 1}`;
-            setItems([...items, newItem]);
-        }
+    const addItem = () => {
+        const newItem = `Item ${items.length + 1}`;
+        setItems([...items, newItem]);
+    }
 
-
-    return(
-        <>
-                <div>
+    return (
+        <div>
             <ul>
                 {items.map((item, index) => (
                     <li key={index}>{item}</li>
@@ -22,9 +18,7 @@ const Test = () =>{
             </ul>
             <button onClick={addItem}>Add Item</button>
         </div>
-        
-        </>
-    )
+    );
 }
 
 
