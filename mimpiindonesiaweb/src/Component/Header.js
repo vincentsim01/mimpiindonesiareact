de7aa8c997935cd8a3bd12b8cc18872381a9e62a
Vincent Kiathadi <vincentkiathadi@gmail.com>
2024-10-13 22:10:31 +0800

    logout

--- mimpiindonesiaweb/src/Component/Header.js
+++ mimpiindonesiaweb/src/Component/Header.js
@@ -2,9 +2,73 @@ import React, { useState,useEffect } from 'react';
 import {useNavigate,Link} from 'react-router-dom';
 import Logo from '../Component/Logo/Logo';
 import './Header.css';
-
+const url = "http://localhost:9120";
 
 const Header = () =>{
+
+    const [userData,setUserData] = useState('');
+    let navigate = useNavigate();
+
+    // useEffect(() => {
+    //     if(sessionStorage.getItem('ltk') != null){
+    //         fetch(`${url}/api/auth/userInfo`,{
+    //             method:'GET',
+    //             headers:{
+    //                 'x-access-token':sessionStorage.getItem('ltk')
+    //             }
+    //         })
+    //         .then((res) => res.json())
+    //         .then((data) => {
+    //             setUserData(data)
+
+    //         })
+    //     }
+    // },[])
+
+
+    const handleLogout = () => {
+        sessionStorage.removeItem('ltk');
+        sessionStorage.removeItem('userInfo');
+        setUserData('');
+        navigate('/')
+    }
+
+    const conditionalHeader = () =>{
+
+        if (sessionStorage.getItem('ltk')){
+
+            return(
+
+
+
+                <div className="HeaderRight">
+                <button onClick={handleLogout}>Logout</button>
+                <Link to="/viewfavcard">Favorite Card</Link>
+                <Link to="/pickfavcard">Favorite Card</Link>
+
+                </div>
+            )
+    
+    
+            } else {
+                return(
+
+                    <div className="HeaderRight">
+                    <Link to="/register">Register</Link>
+                    <Link to="/login">Login</Link>
+                    <Link to="/viewfavcard">Favorite Card</Link>
+                    <Link to="/pickfavcard">Favorite Card</Link>
+            
+                    </div>
+    
+
+                )
+    
+        }
+    }
+
+
+
 
     return(
         <div className="HeaderContainer">
@@ -19,13 +83,8 @@ const Header = () =>{
             <Link to="/Money"><div className="HeaderContent">Money</div></Link>
             <Link to="/"><div className="HeaderContent"><Logo/></div></Link>
             </div>
-            <div className="HeaderRight">
-                <Link to="/register">Register</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/viewfavcard">Favorite Card</Link>
-                <Link to="/pickfavcard">Favorite Card</Link>
- 
-            </div>
+            {conditionalHeader()}
+           
             
         </div>
     )
