db3717d2471574553e0c628dc01ec07ccdc43646
Vincent Kiathadi <vincentkiathadi@gmail.com>
2024-10-19 22:16:39 +0800

    pickfavcard inside event detail card

--- mimpiindonesiaweb/src/Component/EventDetail/EventDetailDisplay.js
+++ mimpiindonesiaweb/src/Component/EventDetail/EventDetailDisplay.js
@@ -1,18 +1,29 @@
 import React,{useState, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
 import {Link} from 'react-router-dom';
 const baseUrl = "http://localhost:9120";
 
 
 const EventDetailDisplay = (props) => {
 
+    let navigate = useNavigate();
+
     const removesessionstorage = () =>{
         sessionStorage.removeItem('cardeventdetail');
     }
+
+
 
     const renderdata = ({listData}) => {
         if(listData){
             if(listData.length>0){   
                 return listData.map((item)=>{
+                    const savesession = () => {
+                        sessionStorage.setItem('cardname',JSON.stringify(item.name));
+                        navigate('/pickfavcard');
+                
+                
+                    }
                     return(
                         <>
                                  {item.name}
@@ -21,6 +32,7 @@ const EventDetailDisplay = (props) => {
                                  <br>
                                  </br>
                                  <Link to="/Event"><button onClick={removesessionstorage}>Go Back Event</button></Link>
+                                 <button className="btn btn-primary" onClick={savesession}>Add as Favorite</button>   
                         </>
                     )
                 })
