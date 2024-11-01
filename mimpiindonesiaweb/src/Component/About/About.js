0417540c4b73704f05e1c6c0899ff89bec3f3032
Vincent Sim <129670790+vincentsim01@users.noreply.github.com>
2024-11-01 20:25:53 +0800

    about

--- mimpiindonesiaweb/src/Component/About/About.js
+++ mimpiindonesiaweb/src/Component/About/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './About.css';
 
 
 
@@ -12,8 +13,19 @@ const About = () => {
 
     return(
         <>
-            <div>
-                About Mimpi Indonesia
+            <div className="aboutuscontainer container">
+                
+                <div className="aboutustitle">
+                    <h1>About Mimpi Indonesia</h1>
+                </div>
+
+                    <div className="aboutuscontent">
+                        Mimpi Indonesia adalah permainan kartu asli Indonesia yang dirancang untuk membawa pemainnya mengejar mimpi-mimpi besar mereka—meskipun hanya di dunia kartu. <br></br><br></br>
+                
+                        Melalui permainan ini, para pemain diajak untuk menyusun strategi, mengasah kepintaran, dan mengandalkan sedikit keberuntungan untuk meraih kemenangan. <br></br><br></br>
+                
+                        Tidak hanya soal kompetisi, Mimpi Indonesia juga menciptakan ruang bagi kebersamaan. Game ini dirancang agar kamu bisa menikmati momen seru bersama teman-teman, berbagi tawa dan cerita sambil merayakan budaya lokal melalui permainan kartu yang autentik. Mimpi Indonesia memberikan pengalaman unik yang tak hanya menghibur, tetapi juga menginspirasi, menjadikan setiap putaran permainan sebagai langkah menuju impian. <br></br>Ayo, raih mimpimu sekarang bersama Mimpi Indonesia!
+                    </div>
             </div>
 
         </>
