d64f343b926c18096c6e7650f713dd4a92c89a7f
Vincent Sim <129670790+vincentsim01@users.noreply.github.com>
2024-11-01 10:53:47 +0800

    action

--- mimpiindonesiaweb/src/Component/ActionDetail/ActionDetail.js
+++ mimpiindonesiaweb/src/Component/ActionDetail/ActionDetail.js
@@ -1,5 +1,6 @@
 import React,{useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import ActionDetailDisplay from './ActionDetailDisplay';
 const baseUrl = "http://localhost:9120";
 
@@ -20,6 +21,42 @@ const ActionDetail = () =>{
 
     },[])
 
+    const Previousbutton = () =>{
+        let navigate = useNavigate();
+
+        function previousaction(){
+            fetch(`${baseUrl}/actiondetail/${sessionData-1}`,{method:'GET'})
+            .then((res)=>res.json())
+            .then((data)=>setactiondetail(data));
+            sessionStorage.setItem('cardactiondetail', JSON.stringify(sessionData-1));
+
+            navigate(`/actiondetail/${sessionData-1}`);
+        }
+
+        return(
+            <button className="btn-btn primary" onClick={previousaction}>Previous</button>
+        )
+
+    }
+
+    const Nextbutton = () =>{
+        let navigate = useNavigate();
+
+        function nextaction(){
+            fetch(`${baseUrl}/actiondetail/${Number(sessionData)+1}`,{method:'GET'})
+            .then((res)=>res.json())
+            .then((data)=>setactiondetail(data));
+            sessionStorage.setItem('cardactiondetail', JSON.stringify(Number(sessionData)+1));
+
+            navigate(`/actiondetail/${Number(sessionData)+1}`);
+        }
+
+        return(
+            <button className="btn-btn primary" onClick={nextaction}>Next</button>
+        )
+
+    }
+
 
 
 
@@ -28,8 +65,11 @@ const ActionDetail = () =>{
 
 
         <>
+        {Previousbutton()}
 
         <ActionDetailDisplay listData={actiondetail}/>
+
+        {Nextbutton()}
         
         
         </>
