c067d4c70f6cf9e6cc467ff27b4cc0cd7222d853
Vincent Sim <129670790+vincentsim01@users.noreply.github.com>
2024-11-01 14:36:51 +0800

    trivia previous next

--- mimpiindonesiaweb/src/Component/TriviaDetail/TriviaDetail.js
+++ mimpiindonesiaweb/src/Component/TriviaDetail/TriviaDetail.js
@@ -1,6 +1,7 @@
 import React,{useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 import TriviaDetailDisplay from './TriviaDetailDisplay';
+import { useNavigate } from 'react-router-dom';
 const baseUrl = "http://localhost:9120";
 
 
@@ -16,12 +17,60 @@ const TriviaDetail = () =>{
 
     },[]);
 
+    const Previousbutton = () =>{
+
+        let navigate = useNavigate();
+
+        function previousevent(){
+            fetch(`${baseUrl}/triviadetail/${sessionData}-1`,{method:'GET'})
+            .then((res)=>res.json())
+            .then((data)=>settriviadetail(data));
+            sessionStorage.setItem('cardtriviadetail', JSON.stringify(sessionData-1));
+
+            navigate(`/triviadetail/${sessionData-1}`);
+
+        };
+
+        return(
+            <>
+            <button className="btn-btn-primary" onClick={previousevent}>Previous</button>
+            
+            </>
+        )
+    }
+
+
+    const Nextbutton = () =>{
+
+        let navigate = useNavigate();
+
+        function nextevent(){
+            fetch(`${baseUrl}/triviadetail/${Number(sessionData)+1}`,{method:'GET'})
+            .then((res)=>res.json())
+            .then((data)=>settriviadetail(data));
+            sessionStorage.setItem('cardtriviadetail', JSON.stringify(Number(sessionData)+1));
+
+            navigate(`/triviadetail/${Number(sessionData)+1}`);
+
+        };
+
+        return(
+            <>
+             <button className="btn-btn-primary" onClick={nextevent}>Next</button>
+            
+            </>
+        )
+    }
+
 
     return(
         <>
         TriviaDetail
+        {Previousbutton()}
 
         <TriviaDetailDisplay listData={triviadetail}/>
+
+        {Nextbutton()}
         
         </>
     )
