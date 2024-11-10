bc05bb21ea6a8a82982267c28e25875334ef54fd
Vincent Sim <129670790+vincentsim01@users.noreply.github.com>
2024-11-10 23:01:34 +0800

    css

--- mimpiindonesiaweb/src/Component/ActionDetail/ActionDetailDisplay.js
+++ mimpiindonesiaweb/src/Component/ActionDetail/ActionDetailDisplay.js
@@ -31,12 +31,17 @@ const ActionDetailDisplay = (props) => {
                     <>
                     <div className="container-fluid ActionDetailDisplayContainer">
                         <div className="imgbgcontainer">
-                            <div className="imgbg">
+
+
+                            {/* <div className="bgimage"></div> */}
+                            {/* <div className="imgbg"> */}
+                                <img src="https://i.ibb.co/8YSDPZC/templatecloudhori.png" className="bgimage"></img>
+                            {/* </div> */}
+
+                            {/* <a href="https://ibb.co/XbtyXw7"><img src="https://i.ibb.co/NsK1x49/templatecloud.png" alt="templatecloud" border="0"></a> */}
+                            {/* <div className="imgbg">
                                 <img src="https://i.ibb.co/X8r31Fk/templatebackaction7399.png" className="bgimage"></img>
-                            </div>
-                            <div className="imgbg">
-                                <img src="https://i.ibb.co/X8r31Fk/templatebackaction7399.png" className="bgimage"></img>
-                            </div>
+                            </div> */}
                             {/* <img src="https://i.ibb.co/X8r31Fk/templatebackaction7399.png"></img>
                             <img src="https://i.ibb.co/X8r31Fk/templatebackaction7399.png"></img> */}
                         </div>
