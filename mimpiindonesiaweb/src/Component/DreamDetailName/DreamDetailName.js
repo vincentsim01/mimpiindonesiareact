edfc371c18cf633a6225c60d1a10e1106a5f87db
Vincent Sim <129670790+vincentsim01@users.noreply.github.com>
2024-11-23 23:12:35 +0800

    update the chevron button

--- mimpiindonesiaweb/src/Component/DreamDetailName/DreamDetailName.js
+++ mimpiindonesiaweb/src/Component/DreamDetailName/DreamDetailName.js
@@ -7,6 +7,7 @@ import './DreamDetailName.css';
 const baseUrl = "http://localhost:9120";
 
 const DreamDetailName = () =>{
+    let navigate = useNavigate();
     let params = useParams();
     let cardnameid = params.dreamname;
     const [Dreamcardname,setDreamcardname]= useState([]);
@@ -19,12 +20,46 @@ const DreamDetailName = () =>{
        .then((data) => setDreamcardname(data));
     },[]);
 
-    console.log(Dreamcardname);
+
+    const previousbutton = () =>{
+
+        function previousevent(){
+                sessionStorage.setItem('carddreamdetail',JSON.stringify(1));
+                navigate(`/dreamdetail/1`)
+        }
+        return(   
+            <>
+            <div className="previousbuttoncontainerdream" onClick={previousevent}> <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+            <img className="previouschevronimg" src="https://i.ibb.co/j3vDCBq/chevron2.png"></img></div>
+             </>    
+        )
+
+    }
+
+    const nextbutton = () =>{
+
+        // let navigate = useNavigate();
+
+
+        function nextevent(){
+                sessionStorage.setItem('carddreamdetail',JSON.stringify(3));
+                navigate(`/dreamdetail/3`) 
+        }
+        return(   
+            <>
+            <div className="nextbuttoncontainerdream" onClick={nextevent}><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+            <img className="nextchevronimg" src="https://i.ibb.co/sK4qSHC/chevron.png"></img></div>
+            </>    
+        )
+    }
 
 
     return(<>
-
+    <div className="dreamdetailcontainer">
     <DreamDetailNameDisplay listData={Dreamcardname}/>
+    <div className="previousbuttondream">{previousbutton()}</div>
+    <div className="nextbuttondream">{nextbutton()}</div>
+    </div>
     
     
     </>)
