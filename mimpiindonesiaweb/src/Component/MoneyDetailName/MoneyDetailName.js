873653e60f069ce23d57b27f605fdd375c061946
Vincent Sim <129670790+vincentsim01@users.noreply.github.com>
2024-11-24 23:06:16 +0800

    money

--- mimpiindonesiaweb/src/Component/MoneyDetailName/MoneyDetailName.js
+++ mimpiindonesiaweb/src/Component/MoneyDetailName/MoneyDetailName.js
@@ -7,6 +7,7 @@ import './MoneyDetailName.css';
 const baseUrl = "http://localhost:9120";
 
 const MoneyDetailName = () =>{
+    let navigate = useNavigate();
     let params = useParams();
     let cardnameid = params.moneyname;
     const [Moneycardname,setMoneycardname]= useState([]);
@@ -17,12 +18,51 @@ const MoneyDetailName = () =>{
        .then((data) => setMoneycardname(data));
     },[]);
 
-    console.log(cardnameid);
+    const Previousbutton = () =>{
+
+
+        function previousevent(){ 
+                sessionStorage.setItem('cardmoneydetail',JSON.stringify(1));
+                navigate(`/moneydetail/1`);
+        }
+
+        return(
+            <>
+            <div className="previousbuttoncontainermoney" onClick={previousevent}>
+                <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                <img className="previouschevronimg" src="https://i.ibb.co/j3vDCBq/chevron2.png"></img>
+                </div>
+            </>
+        )
+    }
+
+    const Nextbutton = () =>{
+        let navigate = useNavigate();
+        function nextevent(){
+                sessionStorage.setItem('cardmoneydetail',JSON.stringify(3));
+                navigate(`/moneydetail/${Number(3)}`);
+        }
+        return(
+            <>
+            <div className="nextbuttoncontainermoney" onClick={nextevent}>
+                <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                <img className="nextchevronimg" src="https://i.ibb.co/sK4qSHC/chevron.png"></img>
+            </div>
+            </>
+        )
+        }
 
 
     return(<>
-
-    <MoneyDetailNameDisplay listData={Moneycardname}/>
+            <div className="moneydetailcontainer container-fluid">
+            <MoneyDetailNameDisplay listData={Moneycardname}/>
+            <div className="previousbuttoncontainer">
+                <div>{Previousbutton()}</div>
+            </div>
+            <div className="nextbuttoncontainer">
+                <div>{Nextbutton()}</div>
+            </div>
+        </div>
     
     
     </>)
